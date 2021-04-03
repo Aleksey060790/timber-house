7aa66f753d630bc0c4f9eea695595f652f3a40dd
Aleksey060790 <64468854+Aleksey060790@users.noreply.github.com>
2021-04-03 21:55:42 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+        <div class="container">
+            <a href="tel:<?php the_field('phone') ?>" class="phone"><?php the_field('phone') ?></a>
+            <div class="footer__email">
+                <span>
+                    <?php the_field('email') ?>
+                </span> 
+            </div>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+
+</body>
+
+</html>
